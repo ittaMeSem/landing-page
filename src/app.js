62b96b4312373f7b0bc5dba3c2f72139d0ebac11
Sami <samiciocan@gmail.com>
2022-07-09 13:27:44 +0300

    active status for navbar in progress

--- src/app.js
+++ src/app.js
@@ -1,59 +1,38 @@
-const header = $("header");
-const navbar = document.getElementById("#navbar");
-const navbarLinks = document.querySelector(".navbar-buttons");
-const sections = $("section");
-const links = document.querySelectorAll(".menuButtons");
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("header ul li");
+window.onscroll = () => {
+  let current = "";
 
-const linkCurrentState = () => {
-  const cur_pos = $(this).scrollTop();
-  sections.each(function () {
-    const top = $(this).offset().top - window.innerHeight / 3;
-    const bottom = top + $(this).outerHeight();
-    if (cur_pos >= top && cur_pos <= bottom) {
-      header.find("a").removeClass("current");
-      let linkName = $(this).attr("class");
-      linkName = linkName.slice(0, linkName.length - 8);
-      header.find(`a[data-page= ${linkName}]`).addClass("current");
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
     }
   });
 };
 
-//Scroll page on click
-$(".menuButtons").on("click", function (e) {
-  e.preventDefault();
-  const headerHeight =
-    window.innerWidth <= 768 ? $(window).height() * 0.099 : 0;
-  const idName = $(this).attr("data-page");
-  $("html, body").animate(
-    {
-      scrollTop: $("#" + idName + "-section").offset(),
-    },
-    1200
-  );
+// Scroll page on click
+$(document).ready(function () {
+  $("a").on("click", function (e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      const hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        100,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
-
-const navSlide = () => {
-  burger.addEventListener("click", () => {
-    // toggle nav
-    nav.classList.toggle("nav-active");
-    if (nav.classList.contains("nav-active")) {
-      nav.style.animation = `navSlide 0.5s forwards`;
-      $("body").addClass("navBar-open");
-    } else {
-      nav.style.animation = `navSlideOut 0.5s`;
-      $("body").removeClass("navBar-open");
-    }
-
-    // Animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navFade 0.5s ${index / 5 + 0.5}s ease forwards`;
-      }
-    });
-  });
-};
-
-window.addEventListener("load", linkCurrentState);
-window.addEventListener("scroll", linkCurrentState);
